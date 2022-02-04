1010219aee32377d8ca4872689b3eff94e06a5ac
Bonnie Lucas <b.lucas1263@gmail.com>
2022-02-04 11:20:09 -0600

    Add 16x16 grid

--- script.js
+++ script.js
@@ -1,81 +1,13 @@
 const container = document.querySelector("#container");
 
-const box1 = document.createElement("div");
-box1.classList.add("box");
-box1.textContent = "One";
-container.appendChild(box1);
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+    };
+};
 
-const box2 = document.createElement("div");
-box2.classList.add("box");
-box2.textContent = "Two";
-container.appendChild(box2);
-
-const box3 = document.createElement("div");
-box3.classList.add("box");
-box3.textContent = "Three";
-container.appendChild(box3);
-
-const box4 = document.createElement("div");
-box4.classList.add("box");
-box4.textContent = "Four";
-container.appendChild(box4);
-
-const box5 = document.createElement("div");
-box5.classList.add("box");
-box5.textContent = "Five";
-container.appendChild(box5);
-
-const box6 = document.createElement("div");
-box6.classList.add("box");
-box6.textContent = "Six";
-container.appendChild(box6);
-
-const box7 = document.createElement("div");
-box7.classList.add("box");
-box7.textContent = "Seven";
-container.appendChild(box7);
-
-const box8 = document.createElement("div");
-box8.classList.add("box");
-box8.textContent = "Eight";
-container.appendChild(box8);
-
-const box9 = document.createElement("div");
-box9.classList.add("box");
-box9.textContent = "Nine";
-container.appendChild(box9);
-
-const box10 = document.createElement("div");
-box10.classList.add("box");
-box10.textContent = "Ten";
-container.appendChild(box10);
-
-const box11 = document.createElement("div");
-box11.classList.add("box");
-box11.textContent = "Eleven";
-container.appendChild(box11);
-
-const box12 = document.createElement("div");
-box12.classList.add("box");
-box12.textContent = "Twelve";
-container.appendChild(box12);
-
-const box13 = document.createElement("div");
-box13.classList.add("box");
-box13.textContent = "Thirteen";
-container.appendChild(box13);
-
-const box14 = document.createElement("div");
-box14.classList.add("box");
-box14.textContent = "Fourteen";
-container.appendChild(box14);
-
-const box15 = document.createElement("div");
-box15.classList.add("box");
-box15.textContent = "Fifteen";
-container.appendChild(box15);
-
-const box16 = document.createElement("div");
-box16.classList.add("box");
-box16.textContent = "Sixteen";
-container.appendChild(box16);
+makeRows(16, 16);
