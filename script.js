6f5e68670a4774de4b3532f0d0e2c705a2bc4a7f
Bonnie Lucas <b.lucas1263@gmail.com>
2022-02-04 15:32:40 -0600

    Add function to clear grid

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const container = document.querySelector("#container");
 
+
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
+        cell.addEventListener("mouseover", changeColor);
         container.appendChild(cell).className = "grid-item";
     };
 };
 
-makeRows(16, 16);
+function changeColor(event) {
+    event.target.style.backgroundColor = "black";
+};
+
+function clearGrid(gridSize = 16) {
+    container.innerHTML = '';
+    gridSize = Number(prompt("How many squares would you like?"));
+
+    if (gridSize > 0 && gridSize <= 100) {
+        makeRows(gridSize, gridSize);
+    } else {
+        prompt("Sorry! Pick a number from 1 to 100");
+    };
+};
