c05f04e6a4bd991cbdb2d67aa01e52a1da15bbd6
Bonnie Lucas <b.lucas1263@gmail.com>
2022-02-04 08:35:20 -0600

    add HTML and JS files

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.createElement("div");
+
+const square1 = document.createElement("div");
+square1.classList.add("square");
+square1.textContent = "One";
+container.appendChild(square1);
